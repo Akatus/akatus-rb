cb4bb100a99df5bafc92b8ff8cef9f7aa5fc748a
Diego Charles <diegocharles@diegocharles.com>
2013-01-28 18:45:15 -0200

    fixed akatus.yml path

--- lib/akatus.rb
+++ lib/akatus.rb
@@ -1,5 +1,5 @@
 module Akatus
-  CONFIG = YAML.load_file(File.join(::Rails.root.to_s, 'config', 'akatus.yml'))[Rails.env]
+  CONFIG = YAML.load_file(File.join('config', 'akatus.yml'))[Rails.env]
   EMAIL = CONFIG['email']
   API_KEY = CONFIG['api_key']
   AKATUS_URI = "#{CONFIG["uri"]}/api/v1"

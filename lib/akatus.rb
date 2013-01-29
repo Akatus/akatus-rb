38c9bd9b5617e821a675498aa336a0e19ad23c45
Diego Charles <diegocharles@diegocharles.com>
2013-01-29 19:06:12 -0200

    autoloading todas as classes

--- lib/akatus.rb
+++ lib/akatus.rb
@@ -1,6 +1,14 @@
+require 'akatus/installments'
+require 'akatus/payment_methods'
+require 'akatus/transactions'
+
 module Akatus
   CONFIG = YAML.load_file(File.join('config', 'akatus.yml'))[Rails.env]
   EMAIL = CONFIG['email']
   API_KEY = CONFIG['api_key']
   AKATUS_URI = "#{CONFIG["uri"]}/api/v1"
+
+  autoload :Installments,     'akatus/installments'
+  autoload :PaymentMethods,   'akatus/payment_methods'
+  autoload :Transactions,     'akatus/transactions'
 end
